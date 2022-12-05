ffd10a69790e0b4e42b0145e48c92cc81bf9798d
LexaLexarLexarex <idlexar@gmail.com>
2022-12-05 21:16:56 +0300

    lol switch to spm

---
+++ BookingAParkingSpace/Package.swift
@@ -0,0 +1,8 @@
+//
+//  Package.swift
+//  BookingAParkingSpace
+//
+//  Created by lexar on 05.12.2022.
+//
+
+import Foundation
--- Pods/Target Support Files/Pods-BookingAParkingSpace/Pods-BookingAParkingSpace-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_BookingAParkingSpace : NSObject
-@end
-@implementation PodsDummy_Pods_BookingAParkingSpace
-@end
